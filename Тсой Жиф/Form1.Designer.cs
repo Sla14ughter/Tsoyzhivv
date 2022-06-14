2ebf39967a670e16d19a5f3233acb34401cff595
Sla14ughter <argmozgiblabr@mail.ru>
2022-06-14 18:01:59 +0500

    Добавьте файлы проекта.

---
+++ Тсой Жиф/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Тсой_Жиф
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.n = new System.Windows.Forms.NumericUpDown();
+            this.m = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 93);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать файл f.txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 221);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Создать файл g.txt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // n
+            // 
+            this.n.Location = new System.Drawing.Point(401, 187);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(52, 26);
+            this.n.TabIndex = 2;
+            // 
+            // m
+            // 
+            this.m.Location = new System.Drawing.Point(248, 187);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(63, 26);
+            this.m.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "m";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 418);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m);
+            this.Controls.Add(this.n);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown n;
+        private System.Windows.Forms.NumericUpDown m;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Тсой Жиф/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Тсой_Жиф
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists("f.txt")) { File.Create("f.txt"); }
+            if (!File.Exists("g.txt")) { File.Create("g.txt"); }
+        }
+
+        Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter streamwriter = new StreamWriter("f.txt");
+            string text = "";
+            for (int i = 0; i < 100; i++)
+            {
+                text += $"{random.Next(100)} ";
+            }
+            streamwriter.WriteLine(text);
+            MessageBox.Show(text);
+            streamwriter.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader streamreader = new StreamReader("f.txt");
+            string text = streamreader.ReadToEnd();
+            string[] num = text.Split();
+            string text1 = "";
+            for (int i = 0; i < 100; i++)
+            {
+                int a = int.Parse(num[i]);
+                if (a % (int)m.Value == 0 & a % (int)n.Value != 0)
+                {
+                    text1 += a.ToString() + " ";
+                }
+            }
+            streamreader.Close();
+            StreamWriter streamwriter = new StreamWriter("g.txt");
+            streamwriter.WriteLine(text1);
+            streamwriter.Close();
+            MessageBox.Show(text1);
+        }
+    }
+}
